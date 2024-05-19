e7a82d4868f1661948236630bf50d70f3205014d
yazdan <yazdan.ghezelbash@gmail.com>
2024-05-19 12:46:44 +0330

    nice job

--- color_range/teyfe rang/Form1.cs
+++ color_range/teyfe rang/Form1.cs
@@ -20,20 +20,20 @@ namespace teyfe_rang
         {
             label5.Text = Convert.ToString(hScrollBar1.Value);
 
-            label1.BackColor = Color.FromArgb(Convert.ToInt16(label5.Text), Convert.ToInt16(label6.Text), Convert.ToInt16(label7.Text));
+            label1.BackColor = Color.FromArgb((byte)hScrollBar1.Value, (byte)hScrollBar2.Value, (byte)hScrollBar3.Value);
+            //label1.BackColor = Color.FromArgb(Convert.ToInt16(label5.Text), Convert.ToInt16(label6.Text), Convert.ToInt16(label7.Text));// in this type cast  we will have type error
         }
-        
 
         private void hScrollBar2_Scroll(object sender, ScrollEventArgs e)
         {
             label6.Text = Convert.ToString(hScrollBar2.Value);
-            label1.BackColor = Color.FromArgb(Convert.ToInt16(label5.Text), Convert.ToInt16(label6.Text), Convert.ToInt16(label7.Text));
+            label1.BackColor = Color.FromArgb((byte)hScrollBar1.Value, (byte)hScrollBar2.Value, (byte)hScrollBar3.Value);
         }
 
         private void hScrollBar3_Scroll(object sender, ScrollEventArgs e)
         {
             label7.Text = Convert.ToString(hScrollBar3.Value);
-            label1.BackColor = Color.FromArgb(Convert.ToInt16(label5.Text), Convert.ToInt16(label6.Text), Convert.ToInt16(label7.Text));
+            label1.BackColor = Color.FromArgb((byte)hScrollBar1.Value, (byte)hScrollBar2.Value, (byte)hScrollBar3.Value);
         }
     }
 }
